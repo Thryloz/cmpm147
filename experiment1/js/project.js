f8bab43b751e2e1757317c17560bf718925501dc
Thryloz <86812547+Thryloz@users.noreply.github.com>
2024-04-06 14:47:13 -0700

    added glitch project

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,50 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+    // create an instance of the class
+  const fillers = {
+    modifier: ["Worthless", "Pathetic", "Broken", "Damaged", "Common", "Rare", "Epic", "Legendary", "Mythical"],
+    weapon: ["Sword", "Mace", "Bow", "Spear", "Shield", "Dagger", "Orb", "Tome", "Stick", "Rock", "Arrow", "Hammer", "Halberd", "Axe"],
+    description: ["Destruction", "Cringe", "Annihilation", "Tripping", "Doom", "Magic", "Flight", "Lightning", "Storms", "???", "Defenestration", "Homework"],
+    location: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
+    price:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"] // yea there's a better way to do this but braincells < 0
+  };
 
-  // call a method on the instance
-  myInstance.myMethod();
+  const template = `At last, the item we've all been waiting and the star of today's auction, found in the depths of $location, I present to you...
+
+  The $modifier $weapon of $description!!!!
+
+  Starting bid at $price,000,000 gold.`
+  // i spent a very sad amount of time trying to figure out how to bold the weapon name and failed
+
+  // STUDENTS: You don't need to edit code below this line.
+
+  const slotPattern = /\$(\w+)/;
+
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+
+    /* global box */
+    box.innerText = story;
+  }
+
+  /* global clicker */
+  clicker.onclick = generate;
+
+  generate();
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
